e4f663cf366395de767a81050283c7d8b737e047
Kristian Pettersen <krismrp@gmail.com>
2018-07-19 08:42:25 +0200

    remove unnecessary code

--- webchat.js
+++ webchat.js
@@ -59,27 +59,26 @@ $(document).ready(function initializeChat() {
       chatButton.onclick = function() {
         var firstName = document.getElementById("first-name").value;
         var lastName = document.getElementById("last-name").value;
-        var agentEmail = document.getElementById("agent-email").value;
 
         // Use getConfig.setConfigProperty() for any web chat configuration property to dynamically set config values.
         webchat.getConfig().setData({
           firstName: firstName,
           lastName: lastName,
-          addressStreet: "64472 Brown Street",
-          addressCity: "Lindgrenmouth",
-          addressPostalCode: "50163-2735",
-          addressState: "FL",
-          phoneNumber: "1-916-892-2045 x293",
-          phoneType: "Cell",
-          customerId: 59606
+          // addressStreet: "64472 Brown Street",
+          // addressCity: "Lindgrenmouth",
+          // addressPostalCode: "50163-2735",
+          // addressState: "FL",
+          // phoneNumber: "1-916-892-2045 x293",
+          // phoneType: "Cell",
+          // customerId: 59606
         });
 
-        //Use the updateConfig function to update batches of properties by passing in a partially filled chatConfig object
-        webchat.updateConfig({
-          agentEmail: "debrah.agent@example.com",
-          locale: "es",
-          welcomeMessage: "Welcome to Example support"
-        });
+        // //Use the updateConfig function to update batches of properties by passing in a partially filled chatConfig object
+        // webchat.updateConfig({
+        //   agentEmail: "debrah.agent@example.com",
+        //   locale: "es",
+        //   welcomeMessage: "Welcome to Example support"
+        // });
 
         // Alternatively, call webchat.renderPopup here. Note that reconnects do not apply to popup chat.
         webchat.renderFrame({
